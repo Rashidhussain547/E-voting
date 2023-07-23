f3171a5dc3430fa5f05c4d45afc31093d5acbab6
aman656 <amansiddiquias123@gmail.com>
2023-07-23 23:35:06 +0500

    first one

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
-import './App.css';
-//import Login from './Pages/Login/Login';
-import Candidate from './Pages/ECscreen/Candidate';
-
+import "./App.css";
+import Login from "./Pages/Login/Login";
+import Candidate from "./Pages/ECscreen/Candidate";
+import { BrowserRouter, Routes, Route, Link, Outlet } from "react-router-dom";
+import { Provider } from "react-redux";
+import { store } from "./store";
 function App() {
   return (
-   
-   //<Login />  
-   <Candidate/>
-   
+    <Provider store={store}>
+      <BrowserRouter>
+        <Routes>
+          <Route exact={true} path="/" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
+    </Provider>
   );
 }
 
--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,31 +1,65 @@
 import React from 'react';
 import './Login.css';
 import Navbar from '../../Components/Navbar';
+import { useState } from 'react';
+import { loginAction } from '../../redux/actions/login.action';
+import { useDispatch,useSelector } from 'react-redux';
+import {Circles} from 'react-loader-spinner'
+
 
 const Login = () => {
+  const [numberc,setNumber] = useState()
+  const [cnicError,setCnicError] = useState(false)
+  const [loading,setLoading] =useState(false)
+  const dispatch = useDispatch()
+
   const submitForm = (e) => {
     e.preventDefault();
-    const nicNumber = e.target.elements.nicNumber.value;
-
-    // Perform validation and login logic here (not implemented in this example).
-    // You can use AJAX or other methods to send the NIC number to the server for validation.
-
-    console.log(`Submitted NIC Number: ${nicNumber}`);
+    if(numberc.length<13){
+      setCnicError(true)
+    }else{
+      setLoading(true)
+      dispatch(loginAction({
+        cnic:numberc
+      })).then((res) => {
+        console.log(res)
+        if (res.payload.success) {
+          // toast.success("Entry added successfully.", {
+          //   position: toast.POSITION.TOP_RIGHT
+        // })
+        } else {
+          // toast.error('An error occured!', {
+          //   position: toast.POSITION.TOP_RIGHT
+        // });
+        }
+        setLoading(false)
+      })
+    }
   };
 
   return (
     <div className="Login">
       <Navbar />
       <div className="login-container">
-        <form className="login-form" onSubmit={submitForm}>
+      {!loading ?  <form className="login-form" onSubmit={submitForm}>
           <input
-            type="text"
-            id="nicNumber"
+
+            onChange={(e)=>{
+              setCnicError(false)
+              setNumber(e.target.value)}}
             placeholder="Enter NIC number"
+            value={numberc}
             required
           />
+          {cnicError &&<p style={{color:"red",margin:0,float:"left"}}>Enter valid cnic</p>}
           <button type="submit">Login</button>
-        </form>
+        </form>:   <Circles
+            type="TailSpin"
+            color="rgb(155, 236, 34)"
+            height={80}
+            width={80}
+            timeout={5000}
+        />}
       </div>
     </div>
   );
