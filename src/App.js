f228130fda7586e58f139da22da19835b385eaec
aman656 <amansiddiquias123@gmail.com>
2023-07-27 10:10:14 +0500

    change

--- src/App.js
+++ src/App.js
@@ -4,12 +4,30 @@ import Candidate from "./Pages/ECscreen/Candidate";
 import { BrowserRouter, Routes, Route, Link, Outlet } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./store";
+import { useState } from "react";
 function App() {
+  const admin = useState(localStorage.getItem("admin"));
+  const isAuthAdmin = localStorage.getItem("isAuthAdmin");
   return (
     <Provider store={store}>
       <BrowserRouter>
         <Routes>
           <Route exact={true} path="/" element={<Login />} />
+          <Route exact={true} path="/admin" element={<Login />} />
+          {isAuthAdmin == true && (
+            <Route exact={true} path="/candid" element={<Candidate />} />
+          )}
+          {admin && (
+            <>
+              <Route exact={true} path="/admin" element={<Login />} />
+              {admin == true && isAuthAdmin == true && (
+                <>
+                  <Route />
+                  <Route />
+                </>
+              )}
+            </>
+          )}
         </Routes>
       </BrowserRouter>
     </Provider>
